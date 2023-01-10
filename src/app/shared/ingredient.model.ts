6812b25d4a04552a71fa24f969fd6a49b05ecc10
Shane Banks <shane.banks02@gmail.com>
2023-01-10 15:40:22 -0600

    finished

---
+++ src/app/shared/ingredient.model.ts
@@ -0,0 +1,12 @@
+export class Ingredient {
+    // public name: string;
+    // public amount: number;
+
+    // constructor(public name: string, public amount: number) {
+    //     this.name = name;
+    //     this.amount = amount;
+    // }
+// OR the below
+    constructor(public name: string, public amount: number) {}
+}
+
